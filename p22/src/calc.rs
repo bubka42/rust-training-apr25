0e5b03946c69d5db18067d6d9d532ea1e2103bd0
Ritam Bhaumik <ritam.bhaumik@tii.ae>
2025-04-15 12:18:33 +0400

    Create crate p22 and add calc module to it

---
+++ p22/src/calc.rs
@@ -0,0 +1,54 @@
+/// This is a simple calculator program in Rust that performs various calculations.
+
+/// Celsius to Fahrenheit.
+/// ```
+/// let celsius = 25;
+/// let farenheit = p22::calc::celsius2farenheit(celsius);
+/// assert_eq!(farenheit, 77);
+/// ```
+pub fn celsius2farenheit(celsius: i32) -> i32 {
+    (celsius * 9 / 5) + 32
+}
+
+/// Fahrenheit to Celsius.
+/// ```
+/// let farenheit = 77;
+/// let celsius = p22::calc::farenheit2celsius(farenheit);
+/// assert_eq!(celsius, 25);
+/// ```
+pub fn farenheit2celsius(farenheit: i32) -> i32 {
+    (farenheit - 32) * 5 / 9
+}
+
+/// Fibonacci sequence using loop.
+/// ```
+/// let n = 10;
+/// let fib = p22::calc::fibonacci_loop(n);
+/// assert_eq!(fib, 55);
+/// ```
+pub fn fibonacci_loop(n: u32) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    for _ in 0..n {
+        b += a;
+        a = b - a;
+    }
+    a
+}
+
+/// Fibonacci sequence using recursion.
+/// ```
+/// let n = 10;
+/// let fib = p22::calc::fibonacci_rec(n);
+/// assert_eq!(fib, 55);
+/// ```
+pub fn fibonacci_rec(n: u32) -> u64 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
+    }
+}
